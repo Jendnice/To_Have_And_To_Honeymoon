a38a5048a61860785c9a75e68622114e72b7ebc5
Jendnice <Pipeline38@hotmail.com>
2020-01-12 21:37:41 -0500

    add test code to application controller, routes.rb, index.html, and index.js to confirm fetch working

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -1,1 +1,9 @@
 console.log("test now")
+
+// const BACKEND_URL = 'http://[::1]:3000/';
+// fetch(`${BACKEND_URL}/test`)
+//   .then(response => response.json())
+//   .then(parsedResponse => console.log(parsedResponse));
+
+
+ 
